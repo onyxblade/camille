bb6152e9d945633887cdad7ef95980577fda32c6
辻彩 <cichol@live.cn>
2023-03-03 22:39:52 -0400

    add Type.alias_of

--- lib/camille/type.rb
+++ lib/camille/type.rb
@@ -1,6 +1,28 @@
 module Camille
   # This class represents all custom types defined by the user.
   class Type < BasicType
+    class NotImplementedError < ::NotImplementedError; end
 
+    attr_reader :underlying
+
+    def initialize
+      raise NotImplementedError.new("Missing `alias_of` definition for type.")
+    end
+
+    def self.alias_of type
+      underlying = Camille::Type.instance(type)
+
+      define_method(:initialize) do
+        @underlying = underlying
+      end
+    end
+
+    def check value
+      @underlying.check value
+    end
+
+    def self.const_missing name
+      Camille::Types.const_get(name)
+    end
   end
 end
--- spec/camille/type_spec.rb
+++ spec/camille/type_spec.rb
@@ -1,4 +1,33 @@
 
-RSpec.describe Camille::Type do
+module CamilleTypeSpec
+  class NewType < Camille::Type
+    alias_of(Number)
+  end
+end
 
+RSpec.describe Camille::Type do
+  describe '.alias_of' do
+    it 'defines type alias' do
+      expect(CamilleTypeSpec::NewType.new.underlying).to be_an_instance_of(Camille::Types::Number)
+    end
+  end
+
+  describe '#check' do
+    it 'uses the underlying type to check' do
+      expect(CamilleTypeSpec::NewType.new.check(1)).to be nil
+      expect(CamilleTypeSpec::NewType.new.check('1')).to be_an_instance_of(Camille::TypeError)
+    end
+  end
+
+  describe '#directly_instantiable?' do
+    it 'returns true' do
+      expect(CamilleTypeSpec::NewType.directly_instantiable?).to be true
+    end
+  end
+
+  describe '#initialize' do
+    it 'raises if no `alias_of` definition available' do
+      expect{Class.new(Camille::Type).new}.to raise_error(Camille::Type::NotImplementedError)
+    end
+  end
 end
