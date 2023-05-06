47b80be120475d716aca648a100611f15959225c
辻彩 <cichol@live.cn>
2023-05-05 22:38:37 -0300

    allow symbols in string type

--- lib/camille/types/string.rb
+++ lib/camille/types/string.rb
@@ -2,7 +2,7 @@ module Camille
   module Types
     class String < Camille::BasicType
       def check value
-        unless value.is_a? ::String
+        unless value.is_a?(::String) || value.is_a?(Symbol)
           Camille::TypeError.new("Expected string, got #{value.inspect}.")
         end
       end
--- spec/camille/types/string_spec.rb
+++ spec/camille/types/string_spec.rb
@@ -1,11 +1,12 @@
 
 RSpec.describe Camille::Types::String do
   describe '#check' do
-    it 'returns TypeError if value is not a string' do
-      number = described_class.new
-      expect(number.check('string')).to be nil
-      expect(number.check(1)).to be_an_instance_of(Camille::TypeError)
-      expect(number.check(1).basic?).to be true
+    it 'returns TypeError if value is not a string or a symbol' do
+      string = described_class.new
+      expect(string.check('string')).to be nil
+      expect(string.check(1)).to be_an_instance_of(Camille::TypeError)
+      expect(string.check(1).basic?).to be true
+      expect(string.check(:symbol)).to be nil
     end
   end
 end
