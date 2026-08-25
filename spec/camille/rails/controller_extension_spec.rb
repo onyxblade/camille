bc50a8457a99464331ccdad44692e9dd213655df
Sai Asish Y <say.apm35@gmail.com>
2026-08-24 18:11:56 -0700

    fix ArgumentError shadowing the built-in class in controllers
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- spec/camille/rails/controller_extension_spec.rb
+++ spec/camille/rails/controller_extension_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe 'Controller with Camille::Controller' do
       expect(ProductsController.new.camille_schema).to be(Camille::Schemas::Products)
     end
   end
+
+  describe 'ArgumentError' do
+    it 'does not shadow the top-level ArgumentError inside including controllers' do
+      expect {
+        ProductsController.new.raise_plain_argument_error
+      }.to raise_error(::ArgumentError) { |e| expect(e.class).to eq(::ArgumentError) }
+    end
+  end
 end
